a291b59cacedc160a0a85aaa7ad07d75268a8129
Colin Reeder <colin@vpzom.click>
2024-03-25 21:58:01 -0600

    Don't focus anything on tab if nothing is focused
    temporary solution, eventually would make this conditional

--- crates/egui/src/memory.rs
+++ crates/egui/src/memory.rs
@@ -451,20 +451,6 @@ impl Focus {
                 self.id_next_frame = self.last_interested; // frame-delay so gained_focus works
                 self.reset_focus();
             }
-        } else if self.focus_direction == FocusDirection::Next
-            && self.focused_widget.is_none()
-            && !self.give_to_next
-        {
-            // nothing has focus and the user pressed tab - give focus to the first widgets that wants it:
-            self.focused_widget = Some(FocusWidget::new(id));
-            self.reset_focus();
-        } else if self.focus_direction == FocusDirection::Previous
-            && self.focused_widget.is_none()
-            && !self.give_to_next
-        {
-            // nothing has focus and the user pressed Shift+Tab - give focus to the last widgets that wants it:
-            self.focused_widget = self.last_interested.map(FocusWidget::new);
-            self.reset_focus();
         }
 
         self.last_interested = Some(id);
